370b2be576ccc4f5022e1d70f1ce0dcfde4b189d
mcapace <mike.capace@gmail.com>
2025-11-03 12:33:07 -0500

    Fix: Increase negative margin to -80px to move text box much closer to bottles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -199,7 +199,7 @@ export default function Home() {
             maxWidth: '1600px',
             backgroundColor: '#0D0D0D',
             padding: '20px 40px 40px 40px',
-            marginTop: '-20px',
+            marginTop: '-80px',
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
