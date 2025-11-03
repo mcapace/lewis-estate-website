982b79708bc506d2f56b679987fd8ffa9616666e
mcapace <mike.capace@gmail.com>
2025-11-03 14:48:26 -0500

    Add explicit favicon link tags in HTML head for better browser support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
+      <head>
+        <link rel="icon" href="/images/wsfavicon.avif" type="image/avif" />
+        <link rel="shortcut icon" href="/images/wsfavicon.avif" type="image/avif" />
+        <link rel="apple-touch-icon" href="/images/wsfavicon.avif" />
+      </head>
       <body className="font-inter antialiased">
         {children}
       </body>
