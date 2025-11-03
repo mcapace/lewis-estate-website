1ba7354b2bd3d74b091097c9958ea96abcb03192
mcapace <mike.capace@gmail.com>
2025-11-03 12:03:09 -0500

    Fix: Increase Market Watch logo size further to match other publication logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -436,12 +436,12 @@ export default function Home() {
                       style={{width: 'auto', height: 'auto', maxWidth: '100%'}}
                     />
                   </div>
-                  <div style={{marginBottom: '16px', height: 'auto', maxWidth: '200px'}}>
+                  <div style={{marginBottom: '16px', height: 'auto', maxWidth: '280px'}}>
                     <Image
                       src="/pub logos /MarketWatch.png"
                       alt="Market Watch"
-                      width={200}
-                      height={60}
+                      width={280}
+                      height={80}
                       style={{width: 'auto', height: 'auto', maxWidth: '100%'}}
                     />
                   </div>
