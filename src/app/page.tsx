876edc99f68ea06cbdd369bf8f17cb7abefc9267
mcapace <mike.capace@gmail.com>
2025-11-04 14:18:42 -0500

    Update Facebook URL to Lewis Cellars page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -210,7 +210,7 @@ export default function Home() {
                 </svg>
               </a>
               {/* Facebook */}
-              <a href="https://www.facebook.com/photo.php?fbid=893626216121106&id=100064212060235&set=a.648087484008315" target="_blank" rel="noopener noreferrer" style={{width: '40px', height: '40px', border: '1px solid rgba(255, 255, 255, 0.3)', borderRadius: '6px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#FFFFFF', textDecoration: 'none', transition: 'all 0.3s ease'}} className="hover:border-[#D3A737] hover:bg-[#D3A737]">
+              <a href="https://www.facebook.com/LewisCellars?mibextid=wwXIfr" target="_blank" rel="noopener noreferrer" style={{width: '40px', height: '40px', border: '1px solid rgba(255, 255, 255, 0.3)', borderRadius: '6px', display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#FFFFFF', textDecoration: 'none', transition: 'all 0.3s ease'}} className="hover:border-[#D3A737] hover:bg-[#D3A737]">
                 <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
                 </svg>
